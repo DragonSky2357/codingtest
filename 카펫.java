6fc7edc6588f93ab9bdc5a61729c11bb594c6483
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-06-04 11:34:03 +0900

    Update 카펫.java

--- 카펫.java
+++ 카펫.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        // 갈색 격자 갯수가 5000개 이하 및 세로 길이는 3이상
         for (int width = 3; width <= 5000; width++) {
             for (int height = 3; height <= width; height++) {
+                // 갈색 격자 갯수(가로 길이에서 세로 2칸식 제거)
                 int boundary = (width + height - 2) * 2;
                 int cneter = width * height - boundary;
 
